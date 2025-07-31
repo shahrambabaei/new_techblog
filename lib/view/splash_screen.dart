23f459d7ce2f2d6ebaf2cc3b3628094d9f9fecb3
shahram babaei <shahram.b6996@gmail.com>
2025-07-31 23:06:08 +0330

    create splashScreen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:new_techblog/constants/my_color.dart';
 import 'package:new_techblog/gen/assets.gen.dart';
+import 'package:new_techblog/view/main_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then((value) => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainScreen(),
+        )));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(
-          Assets.images.logosplash.path,
-          height: 104,
-          width: 204,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              Assets.images.logosplash.path,
+              height: 100,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            SpinKitFadingCube(
+              color: SolidColors.primaryColor,
+              size: 32.0,
+            )
+          ],
         ),
       ),
     );
